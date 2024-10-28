63bfcd430c748fc992dbc0f95d92e0e7de913705
Joseph Descarreaux <joseph.descarreaux@outlook.com>
2024-10-28 14:06:26 -0400

    Feat: Wrap reusable code within a function and modifie the output

--- annotate_stock.py
+++ annotate_stock.py
@@ -1,4 +1,3 @@
-import sys
 from backtest.data import FetchCharts, Cache
 from datetime import datetime
 import matplotlib.pyplot as plt
@@ -7,62 +6,74 @@ import pandas as pd
 from scipy.signal import find_peaks
 
 pd.set_option('display.max_rows', 200)
+def annotate_tickers(df):
+    ### Generate the mean curve
+    WINDOW_SIZE = 14
 
-# Fetch de stock data of NVIDIA over 20 years
-TICKERS = ['NVDA']
-pipe = FetchCharts(TICKERS) | Cache()
-data = pipe.get(datetime(2000, 1, 1), datetime(2020, 1, 1))
+    # Get close prices
+    close_price = df['Close']
+    close_price.index = pd.to_datetime(close_price.index).date
 
-### Generate the mean curve
-WINDOW_SIZE = 14
+    # Generate all windows
+    moving_average = close_price.rolling(WINDOW_SIZE).mean()
+    moving_average = moving_average.dropna()
+    close_price = close_price[moving_average.index]
 
-# Get close prices
-close_price = data['NVDA']['Close'].iloc[-100:-55]
-close_price.index = pd.to_datetime(close_price.index).date
+    # Split the data point on which are over or under the mean curve
+    over = close_price >= moving_average
 
-# Generate all windows
-moving_average = close_price.rolling(WINDOW_SIZE).mean()
-moving_average = moving_average.dropna()
-close_price = close_price[moving_average.index]
+    t = np.array(over, dtype=np.int8)
+    i = 0
+    inflection_pts = []
 
-# Split the data point on which are over or under the mean curve
-over = close_price >= moving_average
+    while(i < len(t)):
+        val = t[i]
 
-t = np.array(over, dtype=np.int8)
-i = 0
-inflection_pts = []
-while(i < len(t)):
-    val = t[i]
+        if(val == 0):
+            idx = np.argmax(t[i:]) + i if np.all(~t[i:]) else len(t)
+            if(i != idx):
+                arg = close_price.iloc[i:idx].argmin() + i + 1
+                t[i:arg] = 0
+                t[arg:idx] = 1
 
-    if(val == 0):
-        idx = np.argmax(t[i:]) + i if np.all(~t[i:]) else len(t)
-        arg = close_price.iloc[i:idx].argmin() + i + 1
-        t[i:arg] = 0
-        t[arg:idx] = 1
+        else:
+            idx = np.argmin(t[i:]) + i if not np.all(t[i:]) else len(t)
+            arg = close_price.iloc[i:idx].argmax() + i + 1
+            t[i:arg] = 1
+            t[arg:idx] = 0
 
-    else:
-        idx = np.argmin(t[i:]) + i if not np.all(t[i:]) else len(t)
-        arg = close_price.iloc[i:idx].argmax() + i + 1
-        t[i:arg] = 1
-        t[arg:idx] = 0
+        inflection_pts.append(arg)
+        if i != idx: i = idx
+        else: break
 
-    inflection_pts.append(arg)
-    i = idx
+    annotated_price = pd.DataFrame({
+        'Close': close_price,
+        'Anno': t.tolist()
+    })
 
-trend_up = close_price.copy()
-trend_down = close_price.copy()
+    annotated_price['Anno'] = annotated_price['Anno'].replace({1: 'UP', 0: 'DOWN'})
 
-#inflection_pts = [close_price.index[i] for i in inflection_pts]
-trend_up[(t == 0)] = np.nan
-trend_down[(t == 1)] = np.nan
+    return annotated_price, {'close_price': close_price, 'moving_average' : moving_average}
 
-peaks, _ = find_peaks(close_price, distance=100)
-# plot the mean stock price
-plt.figure(figsize=(12, 6))
-plt.plot(moving_average, label = 'Mean Curve')
-plt.plot(trend_down, color='#a80000')
-plt.plot(trend_up, color = '#14a800')
-plt.tick_params(left = False, right = False , labelleft = False ,
-                labelbottom = False, bottom = False)
-plt.title('Overlay of stock and mean set window prices')
-plt.show()
+if __name__ == '__main__':
+    # Fetch de stock data of NVIDIA over 20 years
+    TICKERS = ['ABBV']
+    pipe = FetchCharts(TICKERS) | Cache()
+    data = pipe.get(datetime(2000, 1, 1), datetime(2020, 1, 1))
+    trend, plt_curve = annotate_tickers(df=data[TICKERS[0]])
+
+    trend_up = plt_curve['close_price'].copy()
+    trend_down = plt_curve['close_price'].copy()
+    trend_up[(trend == 0)] = np.nan
+    trend_down[(trend == 1)] = np.nan
+
+    peaks, _ = find_peaks(plt_curve['close_price'], distance=100)
+    # plot the mean stock price
+    plt.figure(figsize=(12, 6))
+    plt.plot(plt_curve['moving_average'], label = 'Mean Curve')
+    plt.plot(trend_down, color='#a80000')
+    plt.plot(trend_up, color = '#14a800')
+    plt.tick_params(left = False, right = False , labelleft = False ,
+                    labelbottom = False, bottom = False)
+    plt.title('Overlay of stock and mean set window prices')
+    plt.show()
