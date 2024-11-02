fc09148044b3629c4f6447d216c4c09d089dc887
Anthony Lavertu <alavertu2@gmail.com>
2024-11-02 09:59:38 -0400

    Feat(New annotation way): new testing way

---
+++ annotate_stockV2.py
@@ -0,0 +1,123 @@
+from backtest.data import FetchCharts, Cache
+from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+from scipy.signal import find_peaks
+
+pd.set_option('display.max_rows', 200)
+
+
+def add_neutral(df, pct : float):
+    """
+    Ajouter l'annotation "Neutral" au jeu de données.
+    :param df: Prix de fermeture et annotation préliminaire des données.
+    :param pct: Pourcentage de variation en dessous duquel on considère neutre.
+    :return: Jeu de données ayant les annotations mise à jour.
+    """
+    chunk_size = 4
+
+    for i in range(df.shape[0] - chunk_size):
+        # Extract a chunk of 4 rows
+        chunk = df.iloc[i:i + chunk_size]
+
+        # Calculate price variations for consecutive rows in the chunk
+        price_diffs = chunk['Close'].pct_change().abs().iloc[1:] > pct
+
+        # Apply 'NEUTRAL' if both conditions are met
+        if price_diffs.all():
+            df.loc[chunk.index[0], 'Anno'] = 'NEUTRAL'
+            df.loc[chunk.index[1], 'Anno'] = 'NEUTRAL'
+
+    return df
+
+def add_neutral_v2(anot: npt.NDArray[bool], dur : int = 7):
+    # Compute the length of each consecutive sequence of True values
+    change_points = np.diff(anot.astype(int)).nonzero()[0] + 1
+    segments = np.split(anot, change_points) # Segmented array
+
+    # Calculate the lengths of each segment and repeat them to form the output array
+    durations = np.concatenate([np.full(len(segment), len(segment)) for segment in segments])
+
+    out = anot.astype(np.int32)
+    out[durations < dur] = 2
+    return out
+
+def annotate_tickers(df):
+    """
+    Annoter les prix de fermeture
+    :param df: jeu de données contenant les prix de fermeture et les dates
+    :return: Jeu de données annoté et information pour les graphiques
+    """
+    WINDOW_SIZE = 14
+
+    # Get close prices
+    close_price = df['Close']
+    close_price.index = pd.to_datetime(close_price.index).date
+
+    # Generate all windows
+    moving_average = close_price.rolling(WINDOW_SIZE).mean()
+    close_price = close_price[moving_average.index]
+
+    # Split the data point on which are over or under the mean curve
+    over = close_price >= moving_average
+
+    t = np.array(over, dtype=np.int8)
+    i = 0
+    inflection_pts = []
+
+    while(i < len(t)):
+        val = t[i]
+
+        if(val == 0):
+            idx = np.argmax(t[i:]) + i if np.all(~t[i:]) else len(t)
+            if(i != idx):
+                arg = close_price.iloc[i:idx].argmin() + i + 1
+                t[i:arg - 1] = 0
+                t[arg - 1:idx] = 1
+
+        else:
+            idx = np.argmin(t[i:]) + i if not np.all(t[i:]) else len(t)
+            arg = close_price.iloc[i:idx].argmax() + i + 1
+            t[i:arg - 1] = 1
+            t[arg - 1:idx] = 0
+
+        inflection_pts.append(arg)
+        if i != idx: i = idx
+        else: break
+
+    annotations = t.astype(bool)
+    # Mise à jour des annotations
+    annotations = add_neutral_v2(annotations, dur=7)
+    return annotations
+
+if __name__ == '__main__':
+    # Fetch de stock data of NVIDIA over 20 years
+    TICKERS = ['AAPL']
+    pipe = FetchCharts(TICKERS) | Cache()
+    data = pipe.get(datetime(2000, 1, 1), datetime(2020, 1, 1))
+    trend, plt_curve = annotate_tickers(df=data[TICKERS[0]])
+    # with pd.option_context('display.max_rows', None, 'display.max_columns',
+    #                        None):
+    #     print(trend)
+    print(trend[trend['Anno'] == 'NEUTRAL'])
+    trend_up = plt_curve['close_price'].copy()
+    trend_down = plt_curve['close_price'].copy()
+    trend_neutral = plt_curve['close_price'].copy()
+
+    trend_up[trend['Anno'] != 'UP'] = np.nan
+    trend_down[trend['Anno'] != 'DOWN'] = np.nan
+    trend_neutral[trend['Anno'] != 'NEUTRAL'] = np.nan
+
+    # peaks, _ = find_peaks(plt_curve['close_price'], distance=100)
+    # plot the mean stock price
+    plt.figure(figsize=(12, 6))
+    plt.plot(plt_curve['moving_average'], label = 'Mean Curve')
+    plt.plot(trend_down, color='#a80000')
+    plt.plot(trend_up, color='#14a800')
+    plt.plot(trend_neutral, color='#808080')
+    plt.tick_params(left = False, right = False , labelleft = False ,
+                    labelbottom = False, bottom = False)
+    plt.title('Overlay of stock and mean set window prices')
+    plt.show()
---
+++ get_trade_dist.py
@@ -0,0 +1,91 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from annotate_stockV2 import annotate_tickers
+from backtest.data import FetchCharts, Cache, JSONCache, FilterNoneCharts, CausalImpute
+from datetime import datetime
+from deep.pipes.finviz import Finviz
+from tqdm import tqdm
+from datetime import datetime, timedelta
+def fetch_snp500():
+    pipe = Finviz(
+        "https://finviz.com/screener.ashx?v=111&f=idx_sp500%2Cipodate_more5",
+        True) | JSONCache() | FetchCharts(progress=True, throttle=1.,
+                                          auto_adjust=False) | Cache() | FilterNoneCharts() | CausalImpute()
+    pipe.set_id(0)
+    data = pipe.get(datetime(2000, 1, 1), datetime(2020, 1, 1))
+    return data
+
+data = fetch_snp500()
+out = {}
+for ticker, chart in tqdm(data.items(), desc="Computing performances"): # data.items(): #
+    o, c, index = chart['Open'].values, chart['Close'].values, chart.index
+    s = datetime.now()
+    label = annotate_tickers(chart)
+    # change = label[1:] != label[:-1] # Future, pass
+    # idx = np.arange(len(change))[change]
+
+    state = label[0]
+    buy_price = None
+    buy_time = None
+    transactions = []
+    durations = []
+    for i in range(len(label) - 1):
+        if label[i] != state:
+            if label[i] == 1 and buy_price is None: # Buy
+                buy_price = o[i + 1]
+                buy_time = index[i + 1]
+                state = 1.
+            elif (label[i] == 0 or label[i] == 2) and buy_price is not None: # Sell
+                sell_price = o[i + 1]
+                sell_time = index[i + 1]
+                # print(f"Buy: {buy_price}, Sell: {sell_price}")
+                if buy_price != 0.:
+                    transactions.append((sell_price - buy_price) / buy_price)
+                    durations.append((sell_time - buy_time).days)
+                buy_price = None
+                state = label[i]
+        else:
+            continue
+    transactions = np.array(transactions)
+    # Color the curve according to the label
+    # print(np.sum(transactions > 0) / len(transactions))
+    # print(transactions)
+    # print(durations)
+    # red = c.copy()
+    # red[label != 0] = np.nan
+    # green = c.copy()
+    # green[label != 1] = np.nan
+    # grey = c.copy()
+    # grey[label != 2] = np.nan
+    # plt.plot(index, c, label=ticker, color="black")
+    # plt.scatter(index, red, color='r')
+    # plt.scatter(index, green, color='g')
+    # plt.scatter(index, grey, color='grey')
+    # plt.show()
+    # break
+    out[ticker] = (transactions, durations)
+
+# Flatten all transactions
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+transactions = [t for ticker, (transaction_list, duration_list) in out.items() for t in transaction_list]
+quantile = [5.0, 10.0, 25.0, 50.0, 75.0, 90.0, 95.0]
+percentile = np.percentile(a=transactions, q=quantile)
+
+for i in range(len(quantile)):
+    print(f'Gains Quantile:{quantile[i]} -> {percentile[i]}')
+plt.hist(transactions, bins=100)
+for p in percentile:
+    plt.axvline(x=p, color='black', linestyle="--", lw=1)
+
+plt.subplot(1, 2, 2)
+durations = [t for ticker, (transaction_list, duration_list) in out.items() for t in duration_list]
+quantile = [5.0, 10.0, 25.0, 50.0, 75.0, 90.0, 95.0]
+percentile = np.percentile(a=durations, q=quantile)
+
+for i in range(len(quantile)):
+    print(f'Duration Quantile:{quantile[i]} -> {percentile[i]}')
+plt.hist(durations, bins=100)
+for p in percentile:
+    plt.axvline(x=p, color='black', linestyle="--", lw=1)
+plt.show()
